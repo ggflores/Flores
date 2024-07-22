85e2ad253b6b34db2a72a5a1d16ace7386bc4b69
ggflores <59430831+ggflores@users.noreply.github.com>
2024-07-22 11:56:46 -0700

    Delete Flores_Gabriel_Customer_Engineer_Submission.py

--- Flores_Gabriel_Customer_Engineer_Submission.py
+++
@@ -1,9 +0,0 @@
-def question_1():
-    divisBy2_3 = []
-    for num in range(x,y):
-        if num % 2 == 0 or num % 3 == 0:
-            divisBy2_3.append(num)
-    return divisBy2_3
-x = int(input("Please enter the lower limit of the range: "))
-y = int(input("Please enter the upper limit of the range: "))
-print(question_1())
